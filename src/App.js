b0a3b8622e6bfdd9dbc90845a134676280bf67c5
sivasanger <sivasankarshiva008@gmail.com>
2024-01-02 21:09:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './button';
+import LandPage from './LandPage';
+import "./assets/vendor/bootstrap/css/bootstrap.min.css"
+// import "./assets/vendor/aos/aos.css";
+import "./assets/vendor/bootstrap/css/bootstrap.min.css";
+import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
+import "./assets/vendor/boxicons/css/boxicons.min.css";
+import "./assets/vendor/glightbox/css/glightbox.min.css";
+import "./assets/vendor/remixicon/remixicon.css" ;
+import "./assets/vendor/swiper/swiper-bundle.min.css";
+function App() {
+  return (
+    <div>
+      <LandPage></LandPage>
+
+    </div>
+
+  );
+}
+function Text(){
+  return(
+      <p>hello</p>
+  );
+}
+
+
+export default App;
+
+ 
---
+++ src/button.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function button() {
+  return (
+    <div className="button">
+      <button> hi da</button>
+    </div>
+  );
+}
+
+export default button;
